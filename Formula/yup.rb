801375838e03ec4e906fb8fc92ce2b2b030b4b41
Izan <izan.rmm@gmail.com>
2026-01-12 13:47:17 +0100

    yup 0.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/yup.rb
+++ Formula/yup.rb
@@ -1,8 +1,8 @@
 class Yup < Formula
   desc "Safe, cross-platform updater for development tools"
   homepage "https://github.com/izantech/yup"
-  url "https://github.com/izantech/yup/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  url "https://github.com/izantech/yup/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "f7b70e83c3e4c18d95026dc33091183b59c477bcabd4fb2d863a17c18a742109"
   license "MIT"
   head "https://github.com/izantech/yup.git", branch: "main"
 
